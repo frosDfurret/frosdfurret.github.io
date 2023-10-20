4083809e4c00845d992f349f157a6d2cde746ce4
Daniel Aguilar <dado88@dadoMac.local>
2023-10-20 10:19:38 -0700

    full refresh
    sorry i forgot how to git

--- assets/js/loading.js
+++
@@ -1,3 +0,0 @@
-const messages = ['burning the pancakes...','frying some eggs...','pushing buttons...','sending bits...','dozing off...','watching anime...','being stupid...','hang on a sec?',"where'd my banana go?",'toasting the...toast...','sleeping on the job...']
-const message = messages[Math.floor(Math.random() * messages.length)];
-document.getElementById('b4-loading').innerHTML = message;
