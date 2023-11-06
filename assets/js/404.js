92d61e4bb2ab0363975dd7296a077eb45bb4dc49
Daniel Aguilar <dado88@dadoMac.local>
2023-11-06 10:19:14 -0800

    old site placeholder

---
+++ assets/js/404.js
@@ -0,0 +1,6 @@
+document.addEventListener('keydown', function(event) {
+	window.location = "/"
+});
+document.addEventListener('click', function(event) {
+	window.location = "/"
+});
---
+++ assets/js/nobile.js
@@ -0,0 +1,9 @@
+/*
+if (window.matchMedia("only screen and (max-width: 760px)").matches && window.location['pathname'] == "/home.html") {
+  window.location = "/m/"
+}
+
+if (!window.matchMedia("only screen and (max-width: 760px)").matches && window.location['pathname'] == "/m/") {
+  window.location = "/"
+}
+*/
