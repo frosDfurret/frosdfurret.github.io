cc634f33f689b780f97614f22d4955aebeb2b0f7
Daniel Aguilar <dado88@dadoMac.local>
2023-11-08 09:55:50 -0800

    better dimensions for laptop screens

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,36 +1,35 @@
-header = document.getElementById('header')
-content = document.getElementById('content')
+header = document.getElementById("header");
+content = document.getElementById("content");
 // fsr i have to set bg color using dom first or else it'll look funny
 document.body.style.background = "#ddd";
-current = null
+current = null;
 bgColors = {
-	"about": "#DBEEFA",
-	"socials": "#FFF4D6",
-	"projects": "#FFD6D8",
-	"gallery": "#EEF8DD"
-}
-
+  about: "#DBEEFA",
+  socials: "#FFF4D6",
+  projects: "#FFD6D8",
+  gallery: "#EEF8DD",
+};
 
 function reset() {
-	header.style.paddingTop = '32vh'
-	content.style.opacity = '0%'
-	document.body.style.background = "#ddd";
-	header.addEventListener('transitionend', function(event) {
-		document.getElementById('about').style.display = 'none'
-		document.getElementById('socials').style.display = 'none'
-		document.getElementById('projects').style.display = 'none'
-		document.getElementById('gallery').style.display = 'none'
-		header.removeEventListener('transitionend', arguments.callee)
-	})
+  header.className = "";
+  content.style.opacity = "0%";
+  document.body.style.background = "#ddd";
+  header.addEventListener("transitionend", function (event) {
+    document.getElementById("about").style.display = "none";
+    document.getElementById("socials").style.display = "none";
+    document.getElementById("projects").style.display = "none";
+    document.getElementById("gallery").style.display = "none";
+    header.removeEventListener("transitionend", arguments.callee);
+  });
 }
 
 function setup(sect) {
-	header.style.paddingTop = '100px'
-	content.style.opacity = '100%'
-	document.body.style.background = bgColors[sect]
-	if (current !== null) {
-		document.getElementById(current).style.display = "none"
-	}
-	document.getElementById(sect).style.display = "block"
-	current = sect;
+  header.className = "movehead";
+  content.style.opacity = "100%";
+  document.body.style.background = bgColors[sect];
+  if (current !== null) {
+    document.getElementById(current).style.display = "none";
+  }
+  document.getElementById(sect).style.display = "block";
+  current = sect;
 }
